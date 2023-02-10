81a2a4e0d6e322f2fa21d09c7352056ce4a26735
js <liderja@naver.com>
2023-02-10 15:33:24 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/120802

---
+++ src/programers/level0/두수의합.java
@@ -0,0 +1,16 @@
+package programers.level0;
+
+public class 두수의합 {
+    /**
+     * https://school.programmers.co.kr/learn/courses/30/lessons/120802
+     * Level 0
+     * */
+    public static void main(String[] args) {
+        System.out.println("기대값 : 5 , 결과값 : " + solution(2, 3));
+        System.out.println("기대값 : 102 , 결과값 : " + solution(100, 2));
+    }
+
+    public static int solution(int num1, int num2) {
+        return num1 + num2;
+    }
+}
