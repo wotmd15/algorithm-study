1c83d62d43311ed0556c0d0cba779d1f6b9acdfe
js <liderja@naver.com>
2023-02-14 13:23:47 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/120802

--- src/programers/level0/두수의합.java
+++ src/programers/level0/두수의합.java
@@ -4,6 +4,7 @@ public class 두수의합 {
     /**
      * https://school.programmers.co.kr/learn/courses/30/lessons/120802
      * Level 0
+     * 두수의 합
      * */
     public static void main(String[] args) {
         System.out.println("기대값 : 5 , 결과값 : " + solution(2, 3));
