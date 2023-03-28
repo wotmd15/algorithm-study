5c7b09419262d92b2b5de7b3c9b793ad2665f82e
js <liderja@naver.com>
2023-03-28 13:12:06 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/120853

---
+++ src/programmers/level0/컨트롤제트.java
@@ -0,0 +1,35 @@
+package programmers.level0;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class 컨트롤제트 {
+
+    /**
+     * https://school.programmers.co.kr/learn/courses/30/lessons/120853
+     * Level 0
+     * 컨트롤 제트
+     * */
+    public static void main(String[] args) {
+        System.out.println("기대값 : 4 , 결과값 : " + solution("1 2 Z 3"));
+        System.out.println("기대값 : 100 , 결과값 : " + solution("10 20 30 40"));
+        System.out.println("기대값 : 1 , 결과값 : " + solution("10 Z 20 Z 1"));
+        System.out.println("기대값 : 0 , 결과값 : " + solution("10 Z 20 Z"));
+        System.out.println("기대값 : -3 , 결과값 : " + solution("-1 -2 -3 Z"));
+    }
+
+    public static int solution(String s) {
+        int answer = 0;
+        int before = 0;
+        for(String str : s.split(" ")){
+            if("Z".equals(str)){
+                answer -= before;
+            } else {
+                before = Integer.parseInt(str);
+                answer += before;
+            }
+        }
+        return answer;
+    }
+
+}
