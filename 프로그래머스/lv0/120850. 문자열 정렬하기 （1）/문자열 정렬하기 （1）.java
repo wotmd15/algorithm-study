323c5c37131210b9986a6ab643a3e9399126a355
wotmd15 <liderja@naver.com>
2023-04-08 21:48:23 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 19.44 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,6 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(String my_string) {
+        return Arrays.stream(my_string.replaceAll("[^0-9]","").split("")).sorted().mapToInt(Integer::parseInt).toArray();
+    }
+}
