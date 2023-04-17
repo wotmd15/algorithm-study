97ee972f3ef53339d910beae7b16fd32495ad016
wotmd15 <liderja@naver.com>
2023-04-17 18:06:16 +0900

    [level 0] Title: 중복된 문자 제거, Time: 1.64 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+class Solution {
+    public String solution(String my_string) {
+        return Arrays.stream(my_string.split("")).distinct().collect(Collectors.joining());
+    }
+}
